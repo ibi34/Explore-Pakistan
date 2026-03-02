bb64fdcd41bbfda1919207b3c651221e96d4a3f8
Muhammad Ibrahim <ibitanoli12@gmail.com>
2026-03-02 15:21:11 +0500

    updated something

--- script.js
+++ script.js
@@ -23,24 +23,7 @@
   });
 
 
-const faders = document.querySelectorAll('.fade-up');
 
-const appearOptions = {
-  threshold: 0.2,
-};
-
-const appearOnScroll = new IntersectionObserver(function(entries, observer) {
-  entries.forEach(entry => {
-    if(entry.isIntersecting) {
-      entry.target.classList.add('show');
-      observer.unobserve(entry.target); // fade only once
-    }
-  });
-}, appearOptions);
-
-faders.forEach(fader => {
-  appearOnScroll.observe(fader);
-});
 
 
 
